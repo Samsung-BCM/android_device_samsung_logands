3a5194702ea74fa9cc87589539b593b11346c716
ZIM <zim555@mail.ru>
2016-12-20 20:01:23 +0200

    Fix build logands

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/logands/full_logan.mk)
+$(call inherit-product, device/samsung/logands/full_logands.mk)
 
 # Setup device configuration
 PRODUCT_DEVICE := logands
